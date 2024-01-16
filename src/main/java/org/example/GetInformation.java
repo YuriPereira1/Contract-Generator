2ebad404d660ba6e2b5b3a8f467e77411fc9d4c0
Yuri Fernandes Pereira <115130984+YuriPereira1@users.noreply.github.com>
2024-01-16 20:45:01 -0300

    Change info for testing purpose.

--- src/main/java/org/example/GetInformation.java
+++ src/main/java/org/example/GetInformation.java
@@ -11,19 +11,19 @@ public class GetInformation {
 
     public GetInformation() {
         LinkedHashMap<String, SimpleEntry<String, String>> data = new LinkedHashMap<>();
-        data.put("name", new SimpleEntry<>("nome", "Ana Paula Martins"));
-        data.put("rg", new SimpleEntry<>("RG", "127651817"));
-        data.put("cpf", new SimpleEntry<>("CPF", "012.077.479-80"));
-        data.put("location", new SimpleEntry<>("Endereço", "R Luiz Fernando Pinheiro Lima de Abreu, n° 291, Md 1 Ant 1325 - Sitio Cercado, Curitiba / Paraná"));
-        data.put("cep", new SimpleEntry<>("CEP", "81925-187"));
-        data.put("phoneNumber", new SimpleEntry<>("Telefone", "(41) 9 9997-4463"));
+        data.put("name", new SimpleEntry<>("nome", "Nome completo do individuo"));
+        data.put("rg", new SimpleEntry<>("RG", "000000000"));
+        data.put("cpf", new SimpleEntry<>("CPF", "000.000.000-00"));
+        data.put("location", new SimpleEntry<>("Endereço", "Rua endereço , n° 000, Bairro, Cidade / Estado"));
+        data.put("cep", new SimpleEntry<>("CEP", "80000-000"));
+        data.put("phoneNumber", new SimpleEntry<>("Telefone", "(99) 9 9999-9999"));
         data.put("houseDescription", new SimpleEntry<>("Descrição da casa", "Casa com dois quartos, sala, cozinha, banheiro, garagem coberta"));
-        data.put("houseLocation", new SimpleEntry<>("Endereço da casa", "Travessa particular grandes lagos, nº 177, Jacaranda, Pontal do Paraná / Paraná"));
-        data.put("dateInn", new SimpleEntry<>("Data de entrada", "13/01/2024"));
-        data.put("dateOut", new SimpleEntry<>("Data de saída", "14/01/2024"));
-        data.put("price", new SimpleEntry<>("Valor", "R$480,00 (Quatrocentos e oitenta reais)"));
-        data.put("contractDate", new SimpleEntry<>("Dia de hoje", "12/01/2024"));
-        data.put("maxPerson", new SimpleEntry<>("Número de pessoas", "6"));
+        data.put("houseLocation", new SimpleEntry<>("Endereço da casa", "Rua endereço da casa, nº 999, Bairro, Cidade / Estado"));
+        data.put("dateInn", new SimpleEntry<>("Data de entrada", "00/00/0000"));
+        data.put("dateOut", new SimpleEntry<>("Data de saída", "99/99/9999"));
+        data.put("price", new SimpleEntry<>("Valor", "R$999,00 (Novecentos e noventa e nove reais)"));
+        data.put("contractDate", new SimpleEntry<>("Dia de hoje", "55/55/5555"));
+        data.put("maxPerson", new SimpleEntry<>("Número de pessoas", "5"));
 
         for (Map.Entry<String, SimpleEntry<String, String>> entry : data.entrySet()) {
             System.out.println("Digite o " + entry.getValue().getKey() + ": ");
